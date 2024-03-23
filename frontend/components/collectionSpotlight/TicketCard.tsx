85be67d1c52dad534e2f2a76db55ec5c30051ca6
SiddhantTotade <siddhanttotade.1994@gmail.com>
2024-03-23 13:42:16 +0530

    modify : mobile responsive

--- frontend/components/collectionSpotlight/TicketCard.tsx
+++ frontend/components/collectionSpotlight/TicketCard.tsx
@@ -26,6 +26,14 @@ const responsive = {
     breakpoint: { max: 1290, min: 464 },
     items: 1,
   },
+  miniTablet: {
+    breakpoint: { max: 1300, min: 790 },
+    items: 2,
+  },
+  miniTablet2: {
+    breakpoint: { max: 1030, min: 790 },
+    items: 1,
+  },
   mobile: {
     breakpoint: { max: 464, min: 0 },
     items: 1,
